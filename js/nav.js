832aee92c4c3bb1fe381493de8f6fe837305ea51
Priyanshu <psharanindbd@gmail.com>
2024-01-27 21:12:29 +0530

    helllo

--- js/nav.js
+++ js/nav.js
@@ -1,45 +1,40 @@
 const createNav = () => {
-  let nav = document.querySelector('.navbar');
+    let nav = document.querySelector('.navbar');
 
-  nav.innerHTML = `
-  <nav class="navbar">
-      <!-- LOGO -->
-      <div class="logo">
-          <img src="img/about12.png" class="brand-logo" alt="dei">
-          <div class="logo-name">
-              <p>Faculty of Engineering,<br>DEI (Deemed University)</p>
-          </div>
+    nav.innerHTML = `
+    <nav class="navbar">
+    <!-- LOGO -->
+    <div class="logo">
+        <img src="img/about12.png" class="brand-logo" alt="dei">
+        <div class="logo-name">
+            <p>Faculty of Engingeering,<br>DEI (Deemed University)</p>
+        </div>
+    </div>
+
+    <!-- NAVIGATION MENU -->
+    <ul class="nav-links">
+
+      <!-- USING CHECKBOX HACK -->
+      <input type="checkbox" id="checkbox_toggle" />
+      <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+
+      <!-- NAVIGATION MENUS -->
+      <div class="menu">
+      <li class="link-item"><a href="index.html" class="link">Home</a></li>
+      <li class="link-item"><a href="https://www.dei.ac.in/dei/" class="link" target="_blank">DEI Home</a></li>
+      <li class="link-item"><a href="https://www.dei.ac.in/dei/engineering/" class="link" target="_blank">FoE Main</a></li>
+      <li class="link-item"><a href="https://www.sysi.org/" class="link" target="_blank">Systems Society of India</a></li>
+      <li class="link-item"><a href="https://www.dei.ac.in/dei/edei/" class="link" target="_blank">e-DEI-WWW</a></li>
+      <li class="link-item"><a href="https://vidyaprasar.dei.ac.in/" class="link" target="_blank">Vidya Prasar</a></li>
+      <li class="link-item"><a href="https://www.dei.ac.in/dei/index.php?option=com_content&view=article&id=868&Itemid=410" class="link" target="_blank">D.O.S.A.</a></li>
+      <li class="link-item"><a href="http://admission.dei.ac.in:8085/cms_new/login" class="link" target="_blank">DEI CMS</a></li>
+      <li class="link-item"><a href="img/vision 2031.pdf" target="_blank">Vision 2031</a></li>
+      <li class="link-item"><a href="contact.html" class="link">Contact</a></li>
+      <li class="link-item"><a href="login.html" class="link">My Dashboard</a></li>
       </div>
-
-      <!-- NAVIGATION MENU -->
-      <ul class="nav-links">
-
-          <!-- USING CHECKBOX HACK -->
-          <input type="checkbox" id="checkbox_toggle" />
-          <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-
-          <!-- NAVIGATION MENUS -->
-          <div class="menu">
-              <li class="link-item"><a href="index.html" class="link">Home</a></li>
-              <li class="link-item"><a href="https://www.dei.ac.in/dei/" class="link" target="_blank">DEI Home</a></li>
-              <li class="link-item"><a href="https://www.dei.ac.in/dei/engineering/" class="link" target="_blank">FoE Main</a></li>
-              <li class="link-item"><a href="https://www.sysi.org/" class="link" target="_blank">Systems Society of India</a></li>
-              <li class="link-item"><a href="https://www.dei.ac.in/dei/edei/" class="link" target="_blank">e-DEI-WWW</a></li>
-              <li class="link-item"><a href="https://vidyaprasar.dei.ac.in/" class="link" target="_blank">Vidya Prasar</a></li>
-              <li class="link-item"><a href="https://www.dei.ac.in/dei/index.php?option=com_content&view=article&id=868&Itemid=410" class="link" target="_blank">D.O.S.A.</a></li>
-              <li class="link-item"><a href="http://admission.dei.ac.in:8085/cms_new/login" class="link" target="_blank">DEI CMS</a></li>
-              <li class="link-item"><a href="img/vision 2031.pdf" target="_blank">Vision 2031</a></li>
-              <li class="link-item"><a href="contact.html" class="link">Contact</a></li>
-              <li class="link-item"><a href="login.html" class="link">My Dashboard</a></li>
-          </div>
-      </ul>
-
-      <!-- Bar below navigation -->
-      <div class="nav-bar-below">
-          <!-- Add content or styling for the bar as needed -->
-      </div>
+    </ul>
   </nav>
-  `;
+    `;
 }
 
 createNav();
