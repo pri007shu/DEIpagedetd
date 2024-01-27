be9b5ef3a78af23c24324e6631d4e40f3c69c080
Priyanshu <psharanindbd@gmail.com>
2024-01-27 19:56:06 +0530

    sharan 2

--- js/nav.js
+++ js/nav.js
@@ -23,6 +23,12 @@ const createNav = () => {
       <li class="link-item"><a href="index.html" class="link">Home</a></li>
       <li class="link-item"><a href="https://www.dei.ac.in/dei/" class="link" target="_blank">DEI Home</a></li>
       <li class="link-item"><a href="https://www.dei.ac.in/dei/engineering/" class="link" target="_blank">FoE Main</a></li>
+      <li class="link-item"><a href="https://www.sysi.org/" class="link" target="_blank">Systems Society of India</a></li>
+      <li class="link-item"><a href="https://www.dei.ac.in/dei/edei/" class="link" target="_blank">e-DEI-WWW</a></li>
+      <li class="link-item"><a href="https://vidyaprasar.dei.ac.in/" class="link" target="_blank">Vidya Prasar</a></li>
+      <li class="link-item"><a href="https://www.dei.ac.in/dei/index.php?option=com_content&view=article&id=868&Itemid=410" class="link" target="_blank">D.O.S.A.</a></li>
+      <li class="link-item"><a href="http://admission.dei.ac.in:8085/cms_new/login" class="link" target="_blank">DEI CMS</a></li>
+      <li class="link-item"><a href="img/vision 2031.pdf" target="_blank">Vision 2031</a></li>
       <li class="link-item"><a href="contact.html" class="link">Contact</a></li>
       <li class="link-item"><a href="login.html" class="link">My Dashboard</a></li>
       </div>
