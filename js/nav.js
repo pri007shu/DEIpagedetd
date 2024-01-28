96bdec40686bec0f4255dda9f13a0a808a624c80
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-01-28 23:38:27 +0530

    small update

--- js/nav.js
+++ js/nav.js
@@ -27,7 +27,7 @@ const createNav = () => {
       <li class="link-item"><a href="https://www.dei.ac.in/dei/edei/" class="link" target="_blank">e-DEI-WWW</a></li>
       <li class="link-item"><a href="https://vidyaprasar.dei.ac.in/" class="link" target="_blank">Vidya Prasar</a></li>
       <li class="link-item"><a href="https://www.dei.ac.in/dei/index.php?option=com_content&view=article&id=868&Itemid=410" class="link" target="_blank">D.O.S.A.</a></li>
-      <li class="link-item"><a href="http://admission.dei.ac.in:8085/cms_new/login" class="link" target="_blank">DEI CMS</a></li>
+      <li class="link-item"><a href="http://admission.dei.ac.in:8085/cms_new" class="link" target="_blank">DEI CMS</a></li>
       <li class="link-item"><a href="img/vision 2031.pdf" target="_blank">Vision 2031</a></li>
       <li class="link-item"><a href="contact.html" class="link">Contact</a></li>
       <li class="link-item"><a href="login.html" class="link">My Dashboard</a></li>
