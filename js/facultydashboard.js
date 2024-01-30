5bbbfde2249d921091856a0badda9ef5b124560f
Sajal satsangi <sajalsatsangi2004@gmail.com>
2024-01-30 23:41:13 +0530

    hello moto

--- js/facultydashboard.js
+++ js/facultydashboard.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Get references to each navigation item
+    const dashboardNavItem = document.getElementById('dashboard');
     const guestlecturespeakerNavItem = document.getElementById('guestlecturespeaker');
     const projectsanctionedNavItem = document.getElementById('projectsanctioned');
     const journalsNavItem = document.getElementById('journals');
@@ -31,70 +32,97 @@ document.addEventListener('DOMContentLoaded', function() {
         reportContainer6.style.display = 'none';
         reportContainer7.style.display = 'none';
         reportContainer8.style.display = 'none';
+        dashboardNavItem.style.borderLeft = 'none';
+        guestlecturespeakerNavItem.style.borderLeft = 'none';
+        projectsanctionedNavItem.style.borderLeft = 'none';
+        journalsNavItem.style.borderLeft = 'none';
+        bookspublishedNavItem.style.borderLeft = 'none';
+        conferencesNavItem.style.borderLeft = 'none';
+        workshopsorganisedNavItem.style.borderLeft = 'none';
+        foreignvisitsNavItem.style.borderLeft = 'none';
+        otherachievementsNavItem.style.borderLeft = 'none';
     }
 
     // Hide all report containers except reportContainer0 on page load
     hideAllReportContainers();
+    dashboardNavItem.style.borderLeft = '5px solid #010058af';
     reportContainer0.style.display = 'block';
 
-    
 
-    guestlecturespeakerNavItem.addEventListener('click', function() {
+
+    dashboardNavItem.addEventListener('click', function () {
         // Hide all report containers
         hideAllReportContainers();
         // Show the report container associated with articles
-        reportContainer1.style.display = 'block';
+        dashboardNavItem.style.borderLeft = '5px solid #010058af';
+        reportContainer0.style.display = 'block';
     });
 
-    projectsanctionedNavItem.addEventListener('click', function() {
+    guestlecturespeakerNavItem.addEventListener('click', function () {
+        // Hide all report containers
+        hideAllReportContainers();
+        // Show the report container associated with articles
+        guestlecturespeakerNavItem.style.borderLeft = '5px solid #010058af';
+        reportContainer1.style.display = 'block';
+
+    });
+
+    projectsanctionedNavItem.addEventListener('click', function () {
         // Hide all report containers
         hideAllReportContainers();
         // Show the report container associated with report
+        projectsanctionedNavItem.style.borderLeft = '5px solid #010058af';
         reportContainer2.style.display = 'block';
     });
 
-    journalsNavItem.addEventListener('click', function() {
+    journalsNavItem.addEventListener('click', function () {
         // Hide all report containers
         hideAllReportContainers();
         // Show the report container associated with institution
+        journalsNavItem.style.borderLeft = '5px solid #010058af';
         reportContainer3.style.display = 'block';
     });
 
-    
 
-    bookspublishedNavItem.addEventListener('click', function() {
+
+    bookspublishedNavItem.addEventListener('click', function () {
         // Hide all report containers
         hideAllReportContainers();
         // Show the report container associated with profile
+        bookspublishedNavItem.style.borderLeft = '5px solid #010058af';
         reportContainer4.style.display = 'block';
     });
 
-    conferencesNavItem.addEventListener('click', function() {
+    conferencesNavItem.addEventListener('click', function () {
         // Hide all report containers
         hideAllReportContainers();
         // Show the report container associated with profile
+        conferencesNavItem.style.borderLeft = '5px solid #010058af';
         reportContainer5.style.display = 'block';
     });
 
     // Add click event listeners to each navigation item
-    workshopsorganisedNavItem.addEventListener('click', function() {
+    workshopsorganisedNavItem.addEventListener('click', function () {
         // Hide all report containers
         hideAllReportContainers();
         // Show the report container associated with dashboard
+        workshopsorganisedNavItem.style.borderLeft = '5px solid #010058af';
         reportContainer6.style.display = 'block';
     });
 
-    foreignvisitsNavItem.addEventListener('click', function() {
+    foreignvisitsNavItem.addEventListener('click', function () {
         // Hide all report containers
         hideAllReportContainers();
         // Show the report container associated with dashboard
+        foreignvisitsNavItem.style.borderLeft = '5px solid #010058af';
         reportContainer7.style.display = 'block';
     });
 
-    otherachievementsNavItem.addEventListener('click', function() {
+    otherachievementsNavItem.addEventListener('click', function () {
         // Hide all report containers
         hideAllReportContainers();
         // Show the report container associated with dashboard
+        otherachievementsNavItem.style.borderLeft = '5px solid #010058af';
         reportContainer8.style.display = 'block';
     });
 });
